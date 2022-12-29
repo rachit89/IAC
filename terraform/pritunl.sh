0db1b35c3b84dda8d2dfdbd11df9aea2a7d12a69
AyushJakhmola <ayushjakhmola001@gmail.com>
2022-12-29 13:04:12 +0530

    pritunl code

---
+++ terraform/pritunl.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | apt-key add -
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv E162F504A20CDF15827F718D4B7C549A058F8B6B 
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+echo "deb http://repo.pritunl.com/stable/apt focal main" | sudo tee /etc/apt/sources.list.d/pritunl.list
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+apt-get update
+apt-get install mongodb-server pritunl -y
+systemctl start mongodb
+systemctl start pritunl
+key=$(sudo pritunl setup-key) 
+echo "$key" > /home/ubuntu/key
